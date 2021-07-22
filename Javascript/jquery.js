ca3dc93d3dbf894059fb4a315ee8bdfb0fd4bcdc
Judycaicedo <yudy1018@hotmail.com>
2021-07-21 20:21:18 -0500

    Proyecto 4 - Diplomado web

---
+++ Javascript/jquery.js
@@ -0,0 +1,92 @@
+$('.1').click(function(){
+$('.1').css('background', 'yellow');
+})
+
+$('.size').css('font-size', '25px')
+
+$('.color').css('color', 'blue');
+
+$('.2').mouseout(function(){
+    $(this).css('color', 'red')
+    })
+
+$('.tamaño').click(function(){
+$(this).css('font-size', '15px')
+})
+
+$('.resaltar').mouseout(function(){
+$(this).css('color', 'white');
+})
+
+$('.fondo').dblclick(function(){
+$(this).css('background', 'purple');
+})
+
+$('.no-resaltar').click(function(){
+    $(this).css('background', 'pink');
+    });
+
+$(".li5").mouseenter(function(){
+$(this).css("color", "orange");
+});
+
+$('.cambio-color').click(function(){
+$(this).css('color', 'brown');
+})
+
+$(".li6").mouseleave(function(){
+$(this).css("font-size", "30px");
+});
+
+$('.fondo2').mouseover(function(){
+$(this).css('background', 'gray')
+})
+
+$(".li7").mouseenter(function(){
+$(this).css("font-size", "15px");
+});
+
+$('.color-letra').dblclick(function(){
+$(this).css('color', 'blue');
+});
+
+$(".li8").mouseover(function(){
+$(".li8").css("background", "green");
+});
+
+$('.letra-color').css('color', 'brown');
+
+$('.li9').css('font-size', '30px')
+
+$('.tamaño2').mouseout(function(){
+$(this).css('font-size', '14px')
+})
+
+$(".li10").mouseleave(function(){
+$(this).css("color", "pink");
+});
+
+$('.letra').dblclick(function(){
+$(this).css('color', 'white');
+})
+
+
+$('.mom').mouseover(function(){
+$(this).css('background','orange' )
+})
+
+$('.nen').mouseover(function(){
+$(this).css('background', 'pink')
+})
+
+$('.nen').mouseout(function(){
+$(this).css('background', 'gray')
+})
+
+$('.dey').mouseover(function(){
+    $(this).css('background', 'brown')
+    })
+
+$('.dey').mouseout(function(){
+$(this).css('background', 'white')
+})
